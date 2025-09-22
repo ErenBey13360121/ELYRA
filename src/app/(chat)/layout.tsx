03d8c7dafa49aba89fe59656b76821292f19c71e
ErenBey13360121 <erenbey3600@gmail.com>
2025-09-22 06:29:00 +0000

    Clean up remaining auth and db references

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -1,9 +1,8 @@
 import { SidebarProvider } from "ui/sidebar";
 import { AppSidebar } from "@/components/layouts/app-sidebar";
 import { AppHeader } from "@/components/layouts/app-header";
-import { cookies, headers as getHeaders } from "next/headers";
+import { cookies } from "next/headers";
 
-import { auth } from "auth/server";
 import { COOKIE_KEY_SIDEBAR_STATE } from "lib/const";
 import { AppPopupProvider } from "@/components/layouts/app-popup-provider";
 import { SWRConfigProvider } from "./swr-config";
@@ -13,11 +12,7 @@ export const experimental_ppr = true;
 export default async function ChatLayout({
   children,
 }: { children: React.ReactNode }) {
-  const [cookieStore, headers] = await Promise.all([cookies(), getHeaders()]);
-  const session = await auth.api
-    .getSession({
-      headers,
-    })
+  const cookieStore = cookies();
     .catch(() => null);
   const isCollapsed =
     cookieStore.get(COOKIE_KEY_SIDEBAR_STATE)?.value !== "true";
